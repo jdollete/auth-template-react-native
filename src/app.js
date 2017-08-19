5ef867584bff4867b8cf002ee5da24843bb6f922
jdollete <[01;31m[K"email"[m[K: "allain.dollete@gmail.com>
2017-08-18 22:10:30 -0400

    Add firebase configuration

--- src/app.js
+++ src/app.js
@@ -1,8 +1,20 @@
 import React from 'react';
 import { View, Text } from 'react-native';
+import firebase from 'firebase';
 import { Header } from './components/common';
 
 class App extends React.Component {
+
+  componentWillMount() {
+    firebase.initializeApp({
+      apiKey: 'AIzaSyAwdZWuWZx50vWpUPckkQ5gzJVXdExRqlQ',
+      authDomain: 'auth-template-7682c.firebaseapp.com',
+      databaseURL: 'https://auth-template-7682c.firebaseio.com',
+      projectId: 'auth-template-7682c',
+      storageBucket: 'auth-template-7682c.appspot.com',
+      messagingSenderId: '296452916027'
+    });
+  }
 
   render() {
     return (
